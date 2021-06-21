12f2e5bd1309e15a208baee5bd9b26e1cec5a804
mac0616 <76460285+mac0616@users.noreply.github.com>
2021-06-21 19:37:13 +0900

    Add files via upload

---
+++ 2020.09.03 first/src/test/Add.java
@@ -0,0 +1,15 @@
+//202041034_¿Ã√§ø¨
+
+package test;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b=2;
+		int c= a+b;
+		System.out.println(c);
+
+	}
+
+}
---
+++ 2020.09.03 first/src/test/Bye.java
@@ -0,0 +1,13 @@
+package test;
+import java.util.Calendar;
+
+
+public class Bye {
+
+	public static void main(String[] args) {
+		
+		System.out.println("안녀하세요 배화여러분");
+
+	}
+
+}
